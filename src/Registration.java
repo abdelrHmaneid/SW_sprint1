f7bc5ef3e133ee7c2d34a2a742e914981f903d19
abdelrHmaneid <75222067+abdelrHmaneid@users.noreply.github.com>
2021-11-19 23:41:08 +0200

    Registration

--- src/Registration.java
+++ src/Registration.java
@@ -4,6 +4,50 @@ import java.sql.SQLException;
 import java.sql.Statement;
 
 public abstract class  Registration {
+    String userName;
+    String password;
+    String mobileNumber;
+    String email;
+    public Registration(String userName, String password, String mobileNumber, String email) {
+        this.userName = userName;
+        this.password = password;
+        this.mobileNumber = mobileNumber;
+        this.email = email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setMobileNumber(String mobileNumber) {
+        this.mobileNumber = mobileNumber;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getMobileNumber() {
+        return mobileNumber;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+    public abstract void login();
+
 
     public Registration() {
         try {
